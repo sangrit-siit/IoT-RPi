971b29800ea1490ce3d96ce42015b4113124c962
sangrit-siit <Kittikom.San@dome.tu.ac.th>
2018-11-11 18:22:11 +0700

    revert to opencv

--- config.py
+++ config.py
@@ -1,4 +1,40 @@
-PI_MODE = 'entrance'
+from enum import Enum
+import os
 
-API_SERVER_IP = 'http://35.198.217.40/api'
-API_PATH = 'openalpr'
+# ImageAI
+EXECUTION_PATH = os.getcwd()
+
+RASPBERRY_PI_MODE = Enum('RASPBERRY_PI_MODE', 'ENTRANCE EXIT')
+
+OPERATION_MODE = RASPBERRY_PI_MODE.ENTRANCE
+
+MODEL_PATH = os.path.join(EXECUTION_PATH, 'models')
+
+OUTPUT_FILE_PATH = os.path.join(EXECUTION_PATH, 'output')
+
+FRAMES_PER_SECOND = 30
+
+LOG_PROGRESS = True
+
+RETURN_DETECTED_FRAME = True
+
+MINIMUM_PERCENTAGE_PROBABILITY = 30
+
+DISPLAY_PERCENTAGE_PROBABILITY = False
+
+DISDPLAY_OBJECT_NAME = True
+
+SAVE_DETECTED_VIDEO = True
+
+
+# OpenCV
+CLASSIFIER_PATH = os.path.join(EXECUTION_PATH, 'classifier')
+
+SCALE_FACTOR = 1.1
+
+MIN_NEIGHBORS = 1
+
+if __name__ == '__main__':
+    print(dir())
+
+
